668e9a5f716b32be0a99461d1171042be7fe09ad
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-05-28 16:49:09 +0200

    Added art portfolio book and some artworks

---
+++ Javascript/myScripts/artMain.js
@@ -0,0 +1,17 @@
+$(window).ready(function () {
+  $("#portfolio").turn({
+    display: "double",
+    acceleration: true,
+  });
+});
+
+$(window).bind("keydown", function (e) {
+  if (e.keyCode == 37) $("#portfolio").turn("previous");
+  else if (e.keyCode == 39) $("#portfolio").turn("next");
+});
+
+//$(window).bind("start", function (event, pageObject, corner) {
+//if (corner == "tl" || corner == "tr" || corner == "bl" || corner == "br") {
+//event.preventDefault();
+//}
+//});
