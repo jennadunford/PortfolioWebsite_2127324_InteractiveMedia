eb9ba7d1fee8ebff6ac40458a77b5539c843c75e
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-05-31 11:25:02 +0200

    Added javascript navigation for art book

--- Javascript/myScripts/artMain.js
+++ Javascript/myScripts/artMain.js
@@ -11,6 +11,48 @@ $(window).bind("keydown", function (e) {
   else if (e.keyCode == 39) $("#portfolio").turn("next");
 });
 
+$(window).on("click", "#content1", function () {
+  $("#portfolio").turn("page", 2);
+  console.log("clicked first button");
+});
+
+$(window).on("click", "#content2", function () {
+  $("#portfolio").turn("page", 4);
+});
+
+$(window).on("click", "#content3", function () {
+  $("#portfolio").turn("page", 10);
+});
+
+$(window).on("click", "#content4", function () {
+  $("#portfolio").turn("page", 22);
+});
+
+$(window).on("click", "#content5", function () {
+  $("#portfolio").turn("page", 27);
+});
+
+$(window).on("click", "#content6", function () {
+  $("#portfolio").turn("page", 34);
+});
+
+$(window).on("click", "#content7", function () {
+  $("#portfolio").turn("page", 40);
+});
+
+$(window).on("click", "#start", function () {
+  $("#portfolio").turn("page", 1);
+  console.log("clicked first button");
+});
+
+$(window).on("click", "#prev", function () {
+  $("#portfolio").turn("previous");
+});
+
+$(window).on("click", "#next", function () {
+  $("#portfolio").turn("next");
+});
+
 //$(window).bind("start", function (event, pageObject, corner) {
 //if (corner == "tl" || corner == "tr" || corner == "bl" || corner == "br") {
 //event.preventDefault();
