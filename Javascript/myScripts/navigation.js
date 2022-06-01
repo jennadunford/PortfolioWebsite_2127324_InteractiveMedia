00c86e0625b64b2be63dcfbbedd381a146c54f05
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-06-01 17:20:52 +0200

    testing nav fix

--- Javascript/myScripts/navigation.js
+++ Javascript/myScripts/navigation.js
@@ -1,71 +1,88 @@
 $(window).on("click", "#home", function () {
-  window.location.href = "../index.html";
+  window.location.href = "portfoliowebsite_2127324_interactivemedia/index.html";
 });
 
 $(window).on("click", "#about", function () {
-  window.location.href = "../1_About/about.html";
+  window.location.href =
+    "portfoliowebsite_2127324_interactivemedia/1_About/about.html";
 });
 
 $(window).on("click", "#art", function () {
-  window.location.href = "../2_Art/art.html";
+  window.location.href =
+    "portfoliowebsite_2127324_interactivemedia/2_Art/art.html";
 });
 
 $(window).on("click", "#blogs", function () {
-  window.location.href = "../3_Blogs/blogHome.html";
+  window.location.href =
+    "portfoliowebsite_2127324_interactivemedia/3_Blogs/blogHome.html";
 });
 
 $(window).on("click", "#design", function () {
-  window.location.href = "../4_Design/designHome.html";
+  window.location.href =
+    "portfoliowebsite_2127324_interactivemedia/4_Design/designHome.html";
 });
 
 $(window).on("click", "#contact", function () {
-  window.location.href = "../5_Contact/contact.html";
+  window.location.href =
+    "portfoliowebsite_2127324_interactivemedia/5_Contact/contact.html";
 });
 
 $(window).on("click", "#wireframes", function () {
-  window.location.href = "../4_Design/wireframes.html";
+  window.location.href =
+    "portfoliowebsite_2127324_interactivemedia/4_Design/wireframes.html";
 });
 
 $(window).on("click", "#decisions", function () {
-  window.location.href = "../4_Design/designDecisions.html";
+  window.location.href =
+    "portfoliowebsite_2127324_interactivemedia/4_Design/designDecisions.html";
 });
 
 $(window).on("click", "#inspirations", function () {
-  window.location.href = "../4_Design/inspirations.html";
+  window.location.href =
+    "portfoliowebsite_2127324_interactivemedia/4_Design/inspirations.html";
 });
 
 $(window).on("click", "#reflections", function () {
-  window.location.href = "../4_Design/reflections.html";
+  window.location.href =
+    "portfoliowebsite_2127324_interactivemedia/4_Design/reflections.html";
 });
 
 $(window).on("click", "#uxui", function () {
-  window.location.href = "../4_Design/uxAndui.html";
+  window.location.href =
+    "portfoliowebsite_2127324_interactivemedia/4_Design/uxAndui.html";
 });
 
 $(window).on("click", "#blog1", function () {
-  window.location.href = "../3_Blogs/blog1.html";
+  window.location.href =
+    "portfoliowebsite_2127324_interactivemedia/3_Blogs/blog1.html";
 });
 
 $(window).on("click", "#blog2", function () {
-  window.location.href = "../3_Blogs/blog2.html";
+  window.location.href =
+    "portfoliowebsite_2127324_interactivemedia/3_Blogs/blog2.html";
 });
 
 $(window).on("click", "#blog3", function () {
-  window.location.href = "../3_Blogs/blog3.html";
+  window.location.href =
+    "portfoliowebsite_2127324_interactivemedia/3_Blogs/blog3.html";
 });
 
 $(window).on("click", "#blog4", function () {
-  window.location.href = "../3_Blogs/blog4.html";
+  window.location.href =
+    "portfoliowebsite_2127324_interactivemedia/3_Blogs/blog4.html";
 });
 
 $(window).on("click", "#blog5", function () {
-  window.location.href = "../3_Blogs/blog5.html";
+  window.location.href =
+    "portfoliowebsite_2127324_interactivemedia/3_Blogs/blog5.html";
 });
 
 $(window).on("click", "#blog6", function () {
-  window.location.href = "../3_Blogs/blog6.html";
+  window.location.href =
+    "portfoliowebsite_2127324_interactivemedia/3_Blogs/blog6.html";
 });
 
 $(window).on("click", "#reflectionBlog", function () {
-  window.location.href = "../3_Blogs/blogReflect.html";
+  window.location.href =
+    "portfoliowebsite_2127324_interactivemedia/3_Blogs/blogReflect.html";
 });
