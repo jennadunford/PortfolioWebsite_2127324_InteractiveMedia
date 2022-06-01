3298145b89484a30e6fe99a7f02c76072978332a
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-06-01 17:12:24 +0200

    Added javascript nav to sub sections

--- Javascript/myScripts/navigation.js
+++ Javascript/myScripts/navigation.js
@@ -21,3 +21,51 @@ $(window).on("click", "#design", function () {
 $(window).on("click", "#contact", function () {
   window.location.href = "../5_Contact/contact.html";
 });
+
+$(window).on("click", "#wireframes", function () {
+  window.location.href = "../4_Design/wireframes.html";
+});
+
+$(window).on("click", "#decisions", function () {
+  window.location.href = "../4_Design/designDecisions.html";
+});
+
+$(window).on("click", "#inspirations", function () {
+  window.location.href = "../4_Design/inspirations.html";
+});
+
+$(window).on("click", "#reflections", function () {
+  window.location.href = "../4_Design/reflections.html";
+});
+
+$(window).on("click", "#uxui", function () {
+  window.location.href = "../4_Design/uxAndui.html";
+});
+
+$(window).on("click", "#blog1", function () {
+  window.location.href = "../3_Blogs/blog1.html";
+});
+
+$(window).on("click", "#blog2", function () {
+  window.location.href = "../3_Blogs/blog2.html";
+});
+
+$(window).on("click", "#blog3", function () {
+  window.location.href = "../3_Blogs/blog3.html";
+});
+
+$(window).on("click", "#blog4", function () {
+  window.location.href = "../3_Blogs/blog4.html";
+});
+
+$(window).on("click", "#blog5", function () {
+  window.location.href = "../3_Blogs/blog5.html";
+});
+
+$(window).on("click", "#blog6", function () {
+  window.location.href = "../3_Blogs/blog6.html";
+});
+
+$(window).on("click", "#reflectionBlog", function () {
+  window.location.href = "../3_Blogs/blogReflect.html";
+});
