8cffdc9eb98c2048e32e6d461a31faec4c23b669
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-06-01 20:38:00 +0200

    kinda navigation fix
    I did kind of fix the javascript navigation, but I am not happy with it because it is kinda brute forcing it to work and now I cannot test the navigation in live server which sucks

--- Javascript/myScripts/navigation.js
+++ Javascript/myScripts/navigation.js
@@ -6,69 +6,86 @@ $(window).on("click", "#home", function () {
 });
 
 $(window).on("click", "#about", function () {
-  window.location.pathname = "../../1_About/about.html";
+  window.location.pathname =
+    "PortfolioWebsite_2127324_InteractiveMedia/1_About/about.html";
 });
 
 $(window).on("click", "#art", function () {
-  window.location.pathname = "../../2_Art/art.html";
+  window.location.pathname =
+    "PortfolioWebsite_2127324_InteractiveMedia/2_Art/art.html";
 });
 
 $(window).on("click", "#blogs", function () {
-  window.location.pathname = "../../3_Blogs/blogHome.html";
+  window.location.pathname =
+    "PortfolioWebsite_2127324_InteractiveMedia/3_Blogs/blogHome.html";
 });
 
 $(window).on("click", "#design", function () {
-  window.location.pathname = "../../4_Design/designHome.html";
+  window.location.pathname =
+    "PortfolioWebsite_2127324_InteractiveMedia/4_Design/designHome.html";
 });
 
 $(window).on("click", "#contact", function () {
-  window.location.pathname = "../../5_Contact/contact.html";
+  window.location.pathname =
+    "PortfolioWebsite_2127324_InteractiveMedia/5_Contact/contact.html";
 });
 
 $(window).on("click", "#wireframes", function () {
-  window.location.pathname = "../../4_Design/wireframes.html";
+  window.location.pathname =
+    "PortfolioWebsite_2127324_InteractiveMedia/4_Design/wireframes.html";
 });
 
 $(window).on("click", "#decisions", function () {
-  window.location.pathname = "../../4_Design/designDecisions.html";
+  window.location.pathname =
+    "PortfolioWebsite_2127324_InteractiveMedia/4_Design/designDecisions.html";
 });
 
 $(window).on("click", "#inspirations", function () {
-  window.location.pathname = "../../4_Design/inspirations.html";
+  window.location.pathname =
+    "PortfolioWebsite_2127324_InteractiveMedia/4_Design/inspirations.html";
 });
 
 $(window).on("click", "#reflections", function () {
-  window.location.pathname = "../../4_Design/reflections.html";
+  window.location.pathname =
+    "PortfolioWebsite_2127324_InteractiveMedia/4_Design/reflections.html";
 });
 
 $(window).on("click", "#uxui", function () {
-  window.location.pathname = "../../4_Design/uxAndui.html";
+  window.location.pathname =
+    "PortfolioWebsite_2127324_InteractiveMedia/4_Design/uxAndui.html";
 });
 
 $(window).on("click", "#blog1", function () {
-  window.location.pathname = "../../3_Blogs/blog1.html";
+  window.location.pathname =
+    "PortfolioWebsite_2127324_InteractiveMedia/3_Blogs/blog1.html";
 });
 
 $(window).on("click", "#blog2", function () {
-  window.location.pathname = "../../3_Blogs/blog2.html";
+  window.location.pathname =
+    "PortfolioWebsite_2127324_InteractiveMedia/3_Blogs/blog2.html";
 });
 
 $(window).on("click", "#blog3", function () {
-  window.location.pathname = "../../3_Blogs/blog3.html";
+  window.location.pathname =
+    "PortfolioWebsite_2127324_InteractiveMedia/3_Blogs/blog3.html";
 });
 
 $(window).on("click", "#blog4", function () {
-  window.location.pathname = "../../3_Blogs/blog4.html";
+  window.location.pathname =
+    "PortfolioWebsite_2127324_InteractiveMedia/3_Blogs/blog4.html";
 });
 
 $(window).on("click", "#blog5", function () {
-  window.location.pathname = "../../3_Blogs/blog5.html";
+  window.location.pathname =
+    "PortfolioWebsite_2127324_InteractiveMedia/3_Blogs/blog5.html";
 });
 
 $(window).on("click", "#blog6", function () {
-  window.location.pathname = "../../3_Blogs/blog6.html";
+  window.location.pathname =
+    "PortfolioWebsite_2127324_InteractiveMedia/3_Blogs/blog6.html";
 });
 
 $(window).on("click", "#reflectionBlog", function () {
-  window.location.pathname = "../../3_Blogs/blogReflect.html";
+  window.location.pathname =
+    "PortfolioWebsite_2127324_InteractiveMedia/3_Blogs/blogReflect.html";
 });
