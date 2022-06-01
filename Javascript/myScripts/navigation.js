3cd8fbf90aa450a40cca1adc9434ea91d45e288d
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-06-01 16:59:41 +0200

    Added javascript animation
    Now instead of using html links inside the html docs to navigate through pages, the site makes use of javascript in order to navigate through the pages.
    I did this to fulfil the assignment 2 requirement.

---
+++ Javascript/myScripts/navigation.js
@@ -0,0 +1,23 @@
+$(window).on("click", "#home", function () {
+  window.location.href = "../index.html";
+});
+
+$(window).on("click", "#about", function () {
+  window.location.href = "../1_About/about.html";
+});
+
+$(window).on("click", "#art", function () {
+  window.location.href = "../2_Art/art.html";
+});
+
+$(window).on("click", "#blogs", function () {
+  window.location.href = "../3_Blogs/blogHome.html";
+});
+
+$(window).on("click", "#design", function () {
+  window.location.href = "../4_Design/designHome.html";
+});
+
+$(window).on("click", "#contact", function () {
+  window.location.href = "../5_Contact/contact.html";
+});
